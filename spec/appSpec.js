b4f51ce2d1d9536d00f5156af7998277dd91a9ca
mortenmathiasen <morten@tranebaerkaeret.dk>
2018-05-14 08:00:52 +0200

    Add Express test to solution

--- spec/appSpec.js
+++ spec/appSpec.js
@@ -6,15 +6,10 @@ describe('Server', () => {
         it('returns a JSON payload', (done) => {
             request(app)
                 .get('/users')
-                .then(response => {
-                    expect(200);
+                .expect((response)=>{
                     expect(response.text).toEqual('respond with a resource');
-                    done();
                 })
-                .catch(error => {
-                    throw(error);
-                })
-            ;
+                .expect(200, done);
         });
     });
 })
